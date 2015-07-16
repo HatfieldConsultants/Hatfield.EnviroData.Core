90c605b3d7887c7510e9e7ba973b9f4b54920b1f
gvassas <guillaume.vassas@gmail.com>
2015-07-16 10:01:58 -0700

    Added SiteRepository and corresponding interface

---
+++ Source/Hatfield.EnviroData.Core/Repositories/SiteRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hatfield.EnviroData.Core.Repositories
+{
+    public class SiteRepository : Repository<Site>, ISiteRepository
+    {
+        public SiteRepository(IDbContext dbContext)
+            : base(dbContext)
+        {
+            
+        }
+    }
+}
